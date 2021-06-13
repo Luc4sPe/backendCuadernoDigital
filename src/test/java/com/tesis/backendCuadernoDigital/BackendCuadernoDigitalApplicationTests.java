833a3b8d8f6bba8df79336423c64d6873b635081
Lucas <luccas.a.pe@gmail.com>
2021-06-13 17:53:47 -0300

    Se agrega el proyecto

---
+++ src/test/java/com/tesis/backendCuadernoDigital/BackendCuadernoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesis.backendCuadernoDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCuadernoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
