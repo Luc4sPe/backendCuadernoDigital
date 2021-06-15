76f9c736f5ef878736bea9c3aab323cb410e44d6
Lucas <luccas.a.pe@gmail.com>
2021-06-15 18:58:44 -0300

    Se completo el RolNombre

--- src/main/java/com/tesis/backendCuadernoDigital/security/service/RolService.java
+++ src/main/java/com/tesis/backendCuadernoDigital/security/service/RolService.java
@@ -1,4 +1,22 @@
 package com.tesis.backendCuadernoDigital.security.service;
 
+import com.tesis.backendCuadernoDigital.security.entity.Rol;
+import com.tesis.backendCuadernoDigital.security.enums.RolNombre;
+import com.tesis.backendCuadernoDigital.security.repository.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
 public class RolService {
+
+    @Autowired
+    RolRepository rolRepository;
+
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return rolRepository.findByRolNombre(rolNombre);
+    }
 }
