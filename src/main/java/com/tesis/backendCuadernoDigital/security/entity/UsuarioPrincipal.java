003cd846d50fc2c680d743c66b9fa87f229655ab
Lucas <luccas.a.pe@gmail.com>
2021-06-15 19:21:08 -0300

    Se corrigió un error.

--- src/main/java/com/tesis/backendCuadernoDigital/security/entity/UsuarioPrincipal.java
+++ src/main/java/com/tesis/backendCuadernoDigital/security/entity/UsuarioPrincipal.java
@@ -26,7 +26,7 @@ public class UsuarioPrincipal implements UserDetails {
         this.authorities = authorities;
     }
 
-    public static UsuarioPrincipal buil(Usuario usuario){
+    public static UsuarioPrincipal build(Usuario usuario){
         List<GrantedAuthority> authorities =
                 usuario.getRoles().stream().map(rol -> new SimpleGrantedAuthority(rol
                 .getRolNombre().name())).collect(Collectors.toList());
