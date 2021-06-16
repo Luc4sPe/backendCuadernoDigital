2e8abfd8de4c6f8a89a309e9c38d130843bdd4cd
Lucas <luccas.a.pe@gmail.com>
2021-06-16 13:50:39 -0300

    Se creo la clase Crear Roles, está solo se ejecuta una sola vez

---
+++ src/main/java/com/tesis/backendCuadernoDigital/crearRol/CreateRoles.java
@@ -0,0 +1,23 @@
+package com.tesis.backendCuadernoDigital.crearRol;
+/*
+import com.tesis.backendCuadernoDigital.security.entity.Rol;
+import com.tesis.backendCuadernoDigital.security.enums.RolNombre;
+import com.tesis.backendCuadernoDigital.security.service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+//Crea los roles, solo se ejecuta una sola vez
+@Component
+public class CreateRoles implements CommandLineRunner {
+
+    @Autowired
+    RolService rolService;
+    @Override
+    public void run(String... args) throws Exception {
+        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+        rolService.save(rolAdmin);
+        rolService.save(rolUser);
+    }
+}
+*/
