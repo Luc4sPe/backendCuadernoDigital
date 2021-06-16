a925c875b3376794f7127ff077b34a0dcaa21435
Lucas <luccas.a.pe@gmail.com>
2021-06-15 23:54:23 -0300

    Se completo la clase JwtDto

---
+++ src/main/java/com/tesis/backendCuadernoDigital/security/dto/JwtDto.java
@@ -0,0 +1,52 @@
+package com.tesis.backendCuadernoDigital.security.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+// cuando hacamos un login nos devuelve el response entity del controlador un json web token
+public class JwtDto {
+
+    private String token;
+    private String bearer = "Bearer";
+    private String nombreUsuairo;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public JwtDto(String token, String nombreUsuairo, Collection<? extends GrantedAuthority> authorities) {
+        this.token = token;
+        this.nombreUsuairo = nombreUsuairo;
+        this.authorities = authorities;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getBearer() {
+        return bearer;
+    }
+
+    public void setBearer(String bearer) {
+        this.bearer = bearer;
+    }
+
+    public String getNombreUsuairo() {
+        return nombreUsuairo;
+    }
+
+    public void setNombreUsuairo(String nombreUsuairo) {
+        this.nombreUsuairo = nombreUsuairo;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+}
