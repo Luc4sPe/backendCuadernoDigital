5c1f8612b905d4bbd71a7a44c12d7acbfb898a89
Lucas <luccas.a.pe@gmail.com>
2022-08-26 11:27:00 -0300

    Se termino La Plantación y Cultivo

---
+++ src/main/java/com/tesis/backendCuadernoDigital/controller/CultivoController.java
@@ -0,0 +1,4 @@
+package com.tesis.backendCuadernoDigital.controller;
+
+public class CultivoController {
+}
---
+++ src/main/java/com/tesis/backendCuadernoDigital/service/PlantacionService.java
@@ -0,0 +1,4 @@
+package com.tesis.backendCuadernoDigital.service;
+
+public class PlantacionService {
+}
