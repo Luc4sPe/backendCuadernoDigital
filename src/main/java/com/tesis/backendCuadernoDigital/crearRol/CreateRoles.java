953ca51b3a44d83259d60c6858fb2d38aa0f09ce
Lucas <luccas.a.pe@gmail.com>
2023-08-09 14:34:38 -0300

    Se creo el método para el usuario administrador por defecto solo se ejecuta una sola vez

--- src/main/java/com/tesis/backendCuadernoDigital/crearRol/CreateRoles.java
+++ src/main/java/com/tesis/backendCuadernoDigital/crearRol/CreateRoles.java
@@ -1,17 +1,26 @@
 package com.tesis.backendCuadernoDigital.crearRol;
-/*
+
 import com.tesis.backendCuadernoDigital.security.entity.Rol;
+import com.tesis.backendCuadernoDigital.security.entity.Usuario;
 import com.tesis.backendCuadernoDigital.security.enums.RolNombre;
 import com.tesis.backendCuadernoDigital.security.service.RolService;
+import com.tesis.backendCuadernoDigital.security.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 //Crea los roles, solo se ejecuta una sola vez
+/*
 @Component
 public class CreateRoles implements CommandLineRunner {
 
     @Autowired
     RolService rolService;
+    @Autowired
+    UsuarioService usuarioService;
+
     @Override
     public void run(String... args) throws Exception {
        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
@@ -25,13 +34,33 @@ public class CreateRoles implements CommandLineRunner {
         rolService.save(rolEncargadoAgricola);
         rolService.save(rolProductor);
         rolService.save(rolGerente);
+
+
+        Usuario usuarioAdmin =  new Usuario();
+        usuarioAdmin.setNombre("Lucas");
+        usuarioAdmin.setApellido("Peña");
+        usuarioAdmin.setDni("32.458.305");
+        usuarioAdmin.setNombreUsuario("admin");
+        usuarioAdmin.setEmail("luccas.a.pe@gmail.com");
+        usuarioAdmin.setTelefono("3825411458");
+        usuarioAdmin.setPassword("12345678");
+        Set<Rol> roles = new HashSet<>();
+        roles.add(rolAdmin);
+        usuarioAdmin.setRoles(roles);
+
+        usuarioService.save(usuarioAdmin);
+
+
+
     }
 
 
 
 }
 
-*/
+ */
+
+
 
 
 
