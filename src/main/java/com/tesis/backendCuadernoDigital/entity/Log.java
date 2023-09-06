419814bb14c0c43605b3dd1c8dd9ccf524ed7ece
Lucas <luccas.a.pe@gmail.com>
2023-09-06 13:05:52 -0300

    Se creo el método obtener dirección mac

--- src/main/java/com/tesis/backendCuadernoDigital/entity/Log.java
+++ src/main/java/com/tesis/backendCuadernoDigital/entity/Log.java
@@ -5,10 +5,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tesis.backendCuadernoDigital.enums.LogAccion;
 import com.tesis.backendCuadernoDigital.security.entity.Usuario;
 import org.hibernate.annotations.CreationTimestamp;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.Enumeration;
 
 @Entity
 public class Log {
@@ -35,6 +40,9 @@ public class Log {
     @NotNull
     private Long objeto_id;
 
+    @NotNull
+    String macc;
+
     public Log() {
     }
 
@@ -44,6 +52,7 @@ public class Log {
         this.logAccion = logAccion;
         this.descripcion = descripcion;
         this.objeto_id = objeto_id;
+
     }
 
 
@@ -95,4 +104,54 @@ public class Log {
     public void setObjeto_id(Long objeto_id) {
         this.objeto_id = objeto_id;
     }
+
+    public String getMacc() {
+        return macc;
+    }
+
+    public void setMacc(String macc) {
+        this.macc = macc;
+    }
+
+    public void obtenerMacc()  {
+        String mac = null;
+
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            Enumeration e = NetworkInterface.getNetworkInterfaces();
+
+            while(e.hasMoreElements()) {
+
+                NetworkInterface n = (NetworkInterface) e.nextElement();
+                Enumeration ee = n.getInetAddresses();
+                while(ee.hasMoreElements()) {
+                    InetAddress i = (InetAddress) ee.nextElement();
+                    if(!i.isLoopbackAddress() && !i.isLinkLocalAddress() && i.isSiteLocalAddress()) {
+                        ip = i;
+                    }
+                }
+            }
+
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+            byte[] mac_byte = network.getHardwareAddress();
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < mac_byte.length; i++) {
+                sb.append(String.format("%02X%s", mac_byte[i], (i < mac_byte.length -1) ? "-" : ""));
+            }
+           // mac = sb.toString();
+          //  System.out.println(mac);
+            this.setMacc(sb.toString());
+        } catch (UnknownHostException e) {
+
+        e.printStackTrace();
+
+    } catch (SocketException e){
+
+        e.printStackTrace();
+
+    }
+        }
+
+
 }
--- src/main/java/com/tesis/backendCuadernoDigital/service/LogService.java
+++ src/main/java/com/tesis/backendCuadernoDigital/service/LogService.java
@@ -62,32 +62,38 @@ public class LogService {
 
     public void guardarLogLoginUsuario(Usuario usuario){
         Log log = new Log(usuario,LogAccion.USUARIO_LOGIN,"Acceso al Sistema",usuario.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void guardarLogCreacionUsuario(Usuario usuarioCreado, Usuario usuarioCreador){
         Log log = new Log(usuarioCreador,LogAccion.USUARIO_CREACION,"Se creo el Usuario : "+usuarioCreado.getNombreUsuario(),usuarioCreado.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void guardarLogErrorLogin(Usuario usuario){
         Log log = new Log(usuario,LogAccion.USUARIO_ERROR_LOGIN,"El Usuario no pudo ingresar al sistema", usuario.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void guardarLogErrorLoginUsuarioInactivo(Usuario usuario){
         Log log = new Log(usuario,LogAccion.USUARIO_ERROR_LOGIN_USUARIO_INACTIVO, "El usuario intento acceder estando inactivo al sistema", usuario.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     //Metodo cambio contraseña
     public void guardarCambioContrasenia(Usuario usuario){
         Log log = new Log(usuario,LogAccion.CAMBIO_CONTRASENIA,"El usuario "+usuario.getNombre()+" cambio la contraseña",usuario.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
     //Actualizar perfil
     public void actualizarPerfil(Usuario usuario){
         Log log = new Log(usuario,LogAccion.ACTUALIZAR_PERFIL,"El usuario "+usuario.getNombre()+" actualizo su perfil",usuario.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -95,6 +101,7 @@ public class LogService {
 
     public void guardarLogBajaUsuarioLuegoNIntentoDeAccesoFAllido(Usuario usuario){
         Log log = new Log(usuario,LogAccion.USUARIO_BAJA,"El usuario fue dado de baja por N intentos fallidos de inicio de sesion",usuario.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -124,17 +131,20 @@ public class LogService {
 
     public void guardarAltaUsuario(Usuario usuarioDadoAlta, Usuario usuarioEncargado){
         Log log = new Log(usuarioEncargado,LogAccion.USUARIO_ALTA,"El UsuarioEncargado : "+usuarioEncargado.getNombreUsuario()+" dio de Alta al Usuario : "+usuarioDadoAlta.getNombreUsuario(),usuarioDadoAlta.getId());
+        log.obtenerMacc();
         logRepository.save(log);
 
     }
 
     public void guardarBajaUsuario(Usuario usuarioDadoBaja, Usuario usuarioEncargado){
         Log log = new Log(usuarioEncargado,LogAccion.USUARIO_BAJA,"El UsuarioEncargado : "+usuarioEncargado.getNombreUsuario()+" dio de Baja al Usuario : "+usuarioDadoBaja.getNombreUsuario(),usuarioDadoBaja.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void guardarModificacionUsuario(Usuario usuarioModificado, Usuario usuarioEncargado){
         Log log = new Log(usuarioEncargado,LogAccion.USUARIO_MODIFICACION," El UsuarioEncargado "+usuarioEncargado.getNombreUsuario()+" Modifico al Usuario "+usuarioModificado.getNombreUsuario(),usuarioModificado.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -142,11 +152,13 @@ public class LogService {
 
     public void guardarAltaCultivo(Cultivo cultivo, Usuario usuarioCreador){
         Log log = new Log(usuarioCreador, LogAccion.CREACIÓN_CULTIVO,"El Encargado Agricola: "+usuarioCreador.getNombreUsuario()+" Creo el Cultivo: "+cultivo.getNombre(),cultivo.getIdCultivo());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarCultivo(Cultivo cultivo, Usuario usuarioModificador){
         Log log = new Log(usuarioModificador, LogAccion.MODIFICACIÓN_CULTIVO,"El Encargado Agricola:  "+usuarioModificador.getNombreUsuario()+" Modifico el Cultivo: "+cultivo.getNombre(), cultivo.getIdCultivo());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -154,11 +166,13 @@ public class LogService {
 
     public void guardarPlantacion(Plantacion plantacion, Usuario usuarioProductor){
         Log log = new Log(usuarioProductor,LogAccion.CREACIÓN_PLANTACIÓN,"El productor: "+usuarioProductor.getNombreUsuario()+" Creao la Plantacion: "+plantacion.getNombreTipoCultivo().getNombre(),plantacion.getIdPlantacion());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarPlantacion(Plantacion plantacion, Usuario usuarioProductor){
         Log log = new Log(usuarioProductor,LogAccion.MODIFICACIÓN_PLANTACIÓN,"El productor: "+usuarioProductor.getNombreUsuario()+" Modifico la plantacion "+plantacion.getNombreTipoCultivo().getNombre(),plantacion.getIdPlantacion());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -166,26 +180,31 @@ public class LogService {
 
     public void guardarCuadro(Cuadro cuadro, Usuario usuarioEncargadoAgricola){
         Log log = new Log(usuarioEncargadoAgricola,LogAccion.ALTA_CUADRO, "El Encargado Agricola: "+usuarioEncargadoAgricola.getNombreUsuario()+" Creao el Cuadro: "+cuadro.getNumeroCuadro(),cuadro.getIdCuadro());
+        log.obtenerMacc();
         logRepository.save(log);
     }
     public void modificarCuadro(Cuadro cuadro, Usuario usuarioEncargadoAgricola){
         Log log = new Log(usuarioEncargadoAgricola,LogAccion.MODIFICACIÓN_CUADRO,"El EngargadoAgricola: "+usuarioEncargadoAgricola.getNombreUsuario()+" Modifico el cuadro: "+cuadro.getNumeroCuadro(),cuadro.getIdCuadro());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void agregarCultivoAnterior(Cuadro cuadro, Usuario usuarioEncargadoAgricola){
         Log log = new Log(usuarioEncargadoAgricola,LogAccion.CREACIÓN_CULTIVO_ANTERIOR,"El EngargadoAgricola: "+usuarioEncargadoAgricola.getNombreUsuario()+" Agrego el Cultivo Anterior: "+cuadro.getCultivoAnterior(),cuadro.getIdCuadro());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
      //Metodos Logs Finca
     public void guardarFinca(Finca finca, Usuario usuarioEcargadoAgricola){
         Log log = new Log(usuarioEcargadoAgricola, LogAccion.CREACIÓN_FINCA,"El Encargado Agricola: "+usuarioEcargadoAgricola.getNombreUsuario()+" Creao la Finca: "+finca.getNombre(),finca.getIdFinca());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarFinca(Finca finca, Usuario usuarioEcargadoAgricola){
         Log log = new Log(usuarioEcargadoAgricola, LogAccion.MODIFICACIÓN_FINCA,"El Encargado Agricola: "+usuarioEcargadoAgricola.getNombreUsuario()+" Modifico la Finca: "+finca.getNombre(),finca.getIdFinca());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -193,11 +212,13 @@ public class LogService {
 
     public void guardarLaborSuelo(LaborSuelo laborSuelo, Usuario productor){
         Log log = new Log(productor,LogAccion.CREACIÓN_LABOR_SUELO," El productor: "+productor.getNombreUsuario()+" Creao la Labor de Suelo: "+laborSuelo.getLabor(),laborSuelo.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarLaborSuelo(LaborSuelo laborSuelo, Usuario productor){
         Log log = new Log(productor,LogAccion.MODIFICACIÓN_LABOR_SUELO," El productor: "+productor.getNombreUsuario()+" Modifico la Labor de Suelo: "+laborSuelo.getLabor(),laborSuelo.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -206,11 +227,13 @@ public class LogService {
 
     public void guardarRiego(Riego riego, Usuario productor){
         Log log = new Log(productor,LogAccion.CREACIÓN_RIEGO," El productor: "+productor.getNombreUsuario()+" Realizo un riego de : "+riego.getDuracionEnHoras(),riego.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarRiego(Riego riego, Usuario productor){
         Log log = new Log(productor,LogAccion.MODIFICACIÓN_RIEGO," El productor: "+productor.getNombreUsuario()+" Modifico el riego de: "+riego.getMilimetrosAplicados(),riego.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -218,11 +241,13 @@ public class LogService {
 
     public void guardarAgroquimico(Agroquimico agroquimico, Usuario encargadoAgricola){
         Log log = new Log(encargadoAgricola,LogAccion.CREACIÓN_AGROQUÍMICO," El Encargado Agricola: "+encargadoAgricola.getNombreUsuario()+" Creo el agroquimico : "+agroquimico.getNombreComercial(),agroquimico.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarAgroquimico(Agroquimico agroquimico, Usuario encargadoAgricola){
         Log log = new Log(encargadoAgricola,LogAccion.MODIFICACIÓN_AGROQUÍMICO," El Encargado Agricola: "+encargadoAgricola.getNombreUsuario()+" Creo el agroquimico : "+agroquimico.getNombreComercial(),agroquimico.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -230,11 +255,13 @@ public class LogService {
 
     public void guardarAplicacionAgroquimico(AplicacionDeAgroquimico agroquimico, Usuario productor){
         Log log = new Log(productor,LogAccion.APLICACIÓN_AGROQUÍMICO," El productor: "+productor.getNombreUsuario()+" Realiso la aplicacion de  : "+agroquimico.getAgroquimico().getNombreComercial(),agroquimico.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarAplicacionAgroquimico(AplicacionDeAgroquimico agroquimico, Usuario productor){
         Log log = new Log(productor,LogAccion.MODIFICACIÓN_APLICACIÓN_AGROQUÍMICO," El productor: "+productor.getNombreUsuario()+" Modifico la aplicación de  : "+agroquimico.getAgroquimico().getNombreComercial(),agroquimico.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -242,22 +269,26 @@ public class LogService {
 
     public void guardarAsesoriaRiego(AsesoriaRiego asesoriaRiego, Usuario productor){
         Log log = new Log(productor,LogAccion.CREACIÓN_ASESORAMIENTO_RIEGO," El Encargado Agricola: "+productor.getNombreUsuario()+" Realizo la asesoria del riego de  : "+asesoriaRiego.getCuadro().getNumeroCuadro(),asesoriaRiego.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarAsesoriaRiego(AsesoriaRiego asesoriaRiego, Usuario productor){
         Log log = new Log(productor,LogAccion.MODIFICAR_ASESORÍA_RIEGO," El Encargado Agricola: "+productor.getNombreUsuario()+" Modifico la asesoria del riego  : "+asesoriaRiego.getCuadro().getNumeroCuadro(),asesoriaRiego.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
 
     public void modificarEstadoAsesoriaRiegoTrue(AsesoriaRiego asesoriaRiego, Usuario productor){
         Log log = new Log(productor,LogAccion.SE_APLICÓ_ASESORÍA_RIEGO," El productor: "+productor.getNombreUsuario()+" Aplicó la asesoría del riego  : "+asesoriaRiego.getCuadro().getNumeroCuadro(),asesoriaRiego.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarEstadoAsesoriaRiegoFalse(AsesoriaRiego asesoriaRiego, Usuario productor){
         Log log = new Log(productor,LogAccion.CANCELO_APLICACIÓN_ASESORIA_RIEGO," El productor: "+productor.getNombreUsuario()+" Canceló la aplicación de la asesoría del riego  : "+asesoriaRiego.getCuadro().getNumeroCuadro(),asesoriaRiego.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
@@ -266,22 +297,28 @@ public class LogService {
 
     public void guardarAsesoriaAgroquimico(AsesoriaAgroquimico asesoriaAgroquimico, Usuario productor){
         Log log = new Log(productor,LogAccion.CREACIÓN_ASESORAMIENTO_AGROQUÍMICO," El Encargado Agricola: "+productor.getNombreUsuario()+" Realizo la asesoría de aplicación de agroquímico de  : "+asesoriaAgroquimico.getCuadro().getNumeroCuadro(),asesoriaAgroquimico.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarAsesoriaAgroquimico(AsesoriaAgroquimico asesoriaAgroquimico, Usuario productor){
         Log log = new Log(productor,LogAccion.MODIFICACIÓN_ASESORAMIENTO_AGROQUÍMICO," El Encargado Agricola: "+productor.getNombreUsuario()+" Modificó la asesoría de la aplicación de agroquímico : "+asesoriaAgroquimico.getCuadro().getNumeroCuadro(),asesoriaAgroquimico.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarEstadoAsesoriaAgroTrue(AsesoriaAgroquimico asesoriaAgroquimico, Usuario productor){
         Log log = new Log(productor,LogAccion.SE_APLIÓ_ASESORÍA_AGROQUÍMICO," El productor: "+productor.getNombreUsuario()+" Aplicó la asesoría del riego  : "+asesoriaAgroquimico.getCuadro().getNumeroCuadro(),asesoriaAgroquimico.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
 
     public void modificarEstadoAsesoriaAgroFalse(AsesoriaAgroquimico asesoriaAgroquimico, Usuario productor){
         Log log = new Log(productor,LogAccion.CANCELO_APLICACIÓN_ASESORÍA_AGROÍMICO," El productor: "+productor.getNombreUsuario()+" Canceló la aplicación de la asesoría del riego  : "+asesoriaAgroquimico.getCuadro().getNumeroCuadro(),asesoriaAgroquimico.getId());
+        log.obtenerMacc();
         logRepository.save(log);
     }
+
+
 
 }
